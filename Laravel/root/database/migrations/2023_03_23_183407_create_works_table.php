c53574b253591a9769f5b8162cecd4710b19c1c5
anna <anna.nouchi@epkotsoftware.co.jp>
2023-04-07 10:16:08 +0900

    databaseのテーブルnull許容変更

--- Laravel/root/database/migrations/2023_03_23_183407_create_works_table.php
+++ Laravel/root/database/migrations/2023_03_23_183407_create_works_table.php
@@ -16,12 +16,12 @@ return new class extends Migration
         Schema::create('works', function (Blueprint $table) {
             $table->id()->comment('ID');
             $table->unsignedBigInteger('user_id')->comment('ユーザーID');
-            $table->string('work_content')->comment('備考');
+            $table->string('work_content')->nullable()->comment('備考');
             $table->string('comment')->nullable()->comment('総務コメント');
             $table->date('date')->unique()->comment('日付');
             $table->time('work_start_time')->comment('出勤時間');
-            $table->time('work_end_time')->comment('退勤時間');
-            $table->time('break_time')->nullable()->comment('休憩時間');
+            $table->time('work_end_time')->nullable()->comment('退勤時間');
+            $table->time('break_time')->nullable()->nullable()->comment('休憩時間');
             $table->integer('status_id')->comment('ステータス');
             $table->timestamp('created_at')->comment('作成日時');
             $table->timestamp('updated_at')->comment('更新日時');
